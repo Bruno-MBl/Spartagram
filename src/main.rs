6e50d83420f64f244f03e6663a16d76e4b263df0
Bruno-MBl <bruno.maciejewski@alumnos.uneatlantico.es>
2024-12-29 23:32:12 +0100

    Optimizado para el uso de webhooks

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ use std::{
 };
 
 use ::serenity::all::{
-    ChannelId, CreateAttachment, CreateWebhook, ExecuteWebhook, GetMessages, Message, MessageId,
+    ChannelId, CreateAttachment, CreateWebhook, EditWebhook, ExecuteWebhook, GetMessages, Message,
+    MessageId, Webhook,
 };
 use poise::serenity_prelude as serenity;
 
@@ -58,49 +59,106 @@ fn guardar_ids(ids: Vec<MessageId>, channel_id: ChannelId, path: &str) -> Result
     Ok(())
 }
 
+#[poise::command(slash_command, prefix_command)]
+async fn start(
+    ctx: Context<'_>,
+    #[description = "ID del canal de origen"] channel_id_string: String,
+) -> Result<(), Error> {
+    let directory = "ids.txt";
+    let channel_id: ChannelId = ChannelId::new(
+        channel_id_string
+            .parse()
+            .expect("Error al parsear input de usuario"),
+    );
+    generate_ids(directory, channel_id);
+    let ids = fs::read_to_string(directory).expect("file not found");
+    let ids: VecDeque<u64> = ids
+        .split("\n")
+        .filter(|id| !id.is_empty())
+        .map(|id| id.parse().expect("Error al convertir a u64"))
+        .collect();
+    let mut vec: Vec<u64> = ids.into();
+    vec.sort();
+    let ids = VecDeque::from(vec);
+
+    for message_id in ids.clone() {
+        println!("--{message_id}")
+    }
+    let builder = CreateWebhook::new("Test");
+    let mut webhook = ctx.channel_id().create_webhook(ctx, builder).await?;
+    for message_id in ids {
+        if message_id == channel_id.get() {
+            continue;
+        }
+        println!("{message_id}");
+        let message = channel_id
+            .message(ctx, message_id)
+            .await
+            .expect("No se ha encontrado el mensaje");
+        let _ = send_as_user(ctx.http(), &mut webhook, &message).await;
+
+        println!("FIN")
+    }
+    webhook.delete(ctx).await.expect("Error al borrar webhook");
+    Ok(())
+}
+
 async fn send_as_user(
     http: &Http,
-    channel_id: ChannelId,
+    webhook: &mut Webhook,
     message: &Message,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    println!("Start, send");
+    let start = std::time::Instant::now();
+
     // Crear un webhook en el canal (puedes usar un webhook existente si ya tienes uno)
     let user = &message.author;
+    let user_name = user
+        .global_name
+        .clone()
+        .or(Some(user.name.clone()))
+        .expect("Error al recibir el nombre de usuario");
+    let duration = start.elapsed();
+    println!("Usuario en {:?}", duration);
+
     let user_avatar_url = user.avatar_url().expect("Error al recivir el avatar");
+    let duration = start.elapsed();
+    println!("Avatar en {:?}", duration);
+
     let url = &message
         .attachments
         .first()
         .expect("Error al recivir el attachment")
         .url;
-    let builder = CreateWebhook::new(&user.name)
-        .name(
-            &user
-                .global_name
-                .clone()
-                .expect("Error al acceder al nombre de usuario"),
-        )
-        .avatar(
-            &CreateAttachment::url(http, &user_avatar_url)
-                .await
-                .expect("Error al recibir el avatar"),
-        );
-    let webhook = channel_id.create_webhook(http, builder).await?;
+    let duration = start.elapsed();
+    println!("URL en {:?}", duration);
+
+    let duration = start.elapsed();
+    println!("Edit en {:?}", duration);
+    let duration = start.elapsed();
+    println!("Webhook en {:?}", duration);
 
     // Ejecutar el webhook con el nombre y avatar del usuario
 
-    let builder = ExecuteWebhook::new().add_file(
-        CreateAttachment::url(http, url)
-            .await
-            .expect("Error al añadir archivo a la envia"),
-    );
+    let builder = ExecuteWebhook::new()
+        .username(user_name.clone())
+        .avatar_url(user_avatar_url.clone())
+        .add_file(
+            CreateAttachment::url(http, url)
+                .await
+                .expect("Error al añadir archivo a la envia"),
+        );
     webhook.execute(http, false, builder).await?;
 
     let fecha: String = format_date_es(&mut message.timestamp.to_string());
-    let builder = ExecuteWebhook::new().content(&fecha);
+    let builder = ExecuteWebhook::new()
+        .username(user_name)
+        .avatar_url(user_avatar_url.clone())
+        .content(&fecha);
     webhook.execute(http, false, builder).await?;
     println!("Mensaje :{}", &fecha);
-
-    // Opcional: Borrar el webhook después de usarlo
-    webhook.delete(http).await?;
+    let duration = start.elapsed();
+    println!("Envio en {:?}", duration);
 
     Ok(())
 }
@@ -142,35 +200,9 @@ fn format_date_es(timestamp: &mut String) -> String {
     format!("{} de {}, {}", dia, meses[mes - 1], anio)
 }
 
-#[poise::command(slash_command, prefix_command)]
-async fn start(
-    ctx: Context<'_>,
-    #[description = "ID del canal de origen"] id: String,
-) -> Result<(), Error> {
-    let directory = "ids.txt";
-    let channel_id: ChannelId =
-        ChannelId::new(id.parse().expect("Error al parsear input de usuario"));
-    generate_ids(directory, channel_id);
-    let ids = fs::read_to_string(directory).expect("file not found");
-    let ids: VecDeque<u64> = ids
-        .split("\n")
-        .filter(|id| !id.is_empty())
-        .map(|id| id.parse().expect("Error al convertir a u64"))
-        .collect();
-
-    for message_id in ids {
-        let message = channel_id
-            .message(ctx, message_id)
-            .await
-            .expect("No se ha encontrado el mensaje");
-        let _ = send_as_user(ctx.http(), ctx.channel_id(), &message).await;
-    }
-    Ok(())
-}
-
 fn generate_ids(path: &str, channel_id: ChannelId) {
     let mut ids: Vec<MessageId> = Vec::new();
-    let dir = fs::read_dir(path).expect("Error al abrir el directorio");
+    let dir = fs::read_dir("imagenes").expect("Error al abrir el directorio");
     for archivo in dir {
         let id: u64 = archivo
             .expect("Error al acceder al archivo")
